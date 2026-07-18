9b699a0a49d1b710db3eee4afee3dd5b75ed1d55
sajidosman24 <sajidosman24@gmail.com>
2026-07-18 17:44:13 +0300

    submitting exercise#4

---
+++ exercise#4/app.js
@@ -0,0 +1,24 @@
+
+// // funtion declaration
+// let name="this is function declaration"
+// console.log(name)
+// function add(a , b){
+//     console.log(a+b)
+// }
+
+// add(5 ,3)
+// add(5 ,5)
+// add(10 ,2)
+// add(10 ,10)
+
+let name="this is function declaration"
+console.log(name)
+
+const add =function(a,b){
+    console.log(a+b)
+}
+
+add(5 ,3)
+add(5 ,5)
+add(10 ,2)
+add(10 ,10)
