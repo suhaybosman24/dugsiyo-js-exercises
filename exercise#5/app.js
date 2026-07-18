633b16023c52965e894189221b2d61baf4f3af17
sajidosman24 <sajidosman24@gmail.com>
2026-07-18 17:44:51 +0300

    sumbiting exercise#5

---
+++ exercise#5/app.js
@@ -0,0 +1,15 @@
+let name="this is exercise its for arrows"
+console.log(name)
+
+// const add =function(a,b){
+//     console.log(a+b)
+// }
+
+const add=(a,b)=>{
+    return a+b;
+}
+
+console.log (add(5 ,3))
+console.log (add(5 ,5))
+console.log (add(10 ,2))
+console.log (add(10 ,10))
