54e030cdf82d553361d8527136e3878d32e9a84f
sajidosman24 <sajidosman24@gmail.com>
2026-07-18 13:46:56 +0300

    submit exexercise#

---
+++ exercise#1/app.js
@@ -0,0 +1,34 @@
+// Create a variable x with a value of 7 and a variable y with a value of 3. Perform all the arithmetic operations and log the results.
+x=7;
+y=3;
+
+console.log(x+y);
+console.log(x-y);
+console.log(x/y)
+console.log(x*y)
+console.log(x%y)
+console.log("pre increment")
+console.log(++x)
+
+console.log("post increment")
+console.log(x++)
+console.log(x)
+
+console.log("pre decrement")
+console.log(--y)
+
+console.log("post decrement")
+console.log(y--)
+console.log(y)
+
+// console.log(pre-incrementa)
+// console.log(++x)
+
+// console.log(--x)
+
+// console.log(post decrement)
+// console.log(x++)
+// console.log(x)
+
+// // negative
+// console.log(x--)
